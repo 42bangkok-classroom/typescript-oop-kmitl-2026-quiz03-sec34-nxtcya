266a6e11054ddcffd5b1f339f58c43cb4bde950c
nxtcya <quinn3nxt@gmail.com>
2026-02-20 18:04:48 +0700

    p01

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,30 @@
-export class Car {}
+export class Car {
+    brand?:string;
+    model?:string;
+
+    constructor(brand?:string,model?:string){
+        this.brand = brand
+        this.model = model
+    }
+
+    start():void{
+        console.log("Toyota Camry is starting...")
+
+    }
+    accelerate():void{
+        console.log("Toyota Camry is accelerating...")
+    }
+    brake():void{
+        console.log("Toyota Camry is braking...")
+
+    }
+    stop():void{
+        console.log("Toyota Camry is stopping...")
+    }
+
+}
+
+
+
+
+
