86f57ac501653d57395f46e8a9b44f7995d24a18
nxtcya <quinn3nxt@gmail.com>
2026-02-20 18:09:43 +0700

    p01

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,29 @@
-export class User {}
+export class User {
+    firstname:string;
+    lastname:string;
+    private _age:string;
+
+    constructor(firstname:string,lastname:string,age:string){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this._age = age;
+    }
+
+    setFirstname(){
+        return this.firstname
+    }
+
+    setLastname(){
+        return this.lastname
+    }
+
+    setAge(){
+        return this._age
+    }
+
+    getFullName(fullname:string):string{
+        return fullname = this.firstname + this.lastname
+        
+    }
+}
+
